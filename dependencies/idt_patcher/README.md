e4781bf259bc6ee73b87556aac127869e8eca01d
Stefano Carnà <stefano.carna.dev@gmail.com>
2020-06-05 12:30:42 +0200

    Remove idt_patcher module

--- dependencies/idt_patcher/README.md
+++
@@ -1,3 +0,0 @@
-# IDT-runtime-patcher
-
-## A Linux Kernel Module to dynamically install new IRQ handlers into the system IDT
--- dependencies/idt_patcher/idt_patcher.h
+++
@@ -1,4 +0,0 @@
-extern int idt_patcher_install_entry(unsigned long handler, unsigned vector, unsigned dpl);
-
-// extern int install_hook_smooth(unsigned long handler, 
-// 	unsigned vector, unsigned dpl);
