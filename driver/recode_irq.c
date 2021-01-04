17f426c07595024d9e5ab2d1fb7568c35e6171c5
Stefano Carnà <stefano.carna.dev@gmail.com>
2021-01-04 19:19:36 +0100

    Beta release
    This commit the following changes:
    - Decoupled PMI handler
    - Decoupled Logger for sample buffering
    - Data structs tuned according to machine configuration
    - Decoupled PMC functionalities
    - Enabled the TUNING state
    - Enabled the PROFILE state

---
+++ driver/recode_irq.c
@@ -0,0 +1,72 @@
+#include "dependencies.h"
+#include "recode.h"
+
+int pmi_recode(void)
+{
+	u64 global;
+	unsigned handled = 0, loops = 0;
+	unsigned long flags = 0;
+
+	atomic_inc(&active_pmis);
+
+	irqs_disabled_flags(flags);
+
+	/* Read the PMCs state */
+	rdmsrl(MSR_CORE_PERF_GLOBAL_STATUS, global);
+
+	/* TODO REMOVE */
+
+	/* Nothing to do here */
+	if (!global) {
+		pr_info("[%u] Got PMI %llx - NO GLOBAL - FIXED1 %llx\n", 
+		smp_processor_id(), global, READ_FIXED_PMC(1));
+		goto no_pmi;
+	}
+
+	/* This IRQ is not originated from PMC overflow */
+	if(!(global & PERF_GLOBAL_CTRL_FIXED1_MASK)) {
+		pr_warn("Something triggered pmc_detection_interrupt line\n\
+			MSR_CORE_PERF_GLOBAL_STATUS: %llx\n", global);
+		goto no_pmi;
+	}
+	
+	/* 
+	 * The current implementation of this function does not
+	 * provide a sliding window for a discrete samples collection.
+	 * If a PMI arises, it means that there is a pmc multiplexing
+	 * request. 	 
+	 */
+
+again:
+	if (++loops > 100) {
+		wrmsrl(MSR_CORE_PERF_GLOBAL_CTRL, 0ULL);
+		pr_warn("[%u] LOOP STUCK - STOP PMCs\n", smp_processor_id());
+		goto end;
+	}
+
+	pmc_evaluate_activity(is_pid_tracked(current->tgid), false);
+
+	handled++;
+
+	WRITE_FIXED_PMC(1, reset_period);
+no_pmi:
+
+	apic_write(APIC_LVTPC, RECODE_PMI);
+
+	if (global) {
+		wrmsrl(MSR_CORE_PERF_GLOBAL_OVF_CTRL, global);
+	} else { 
+		/* See arc/x86/intel/events/core.c:intel_pmu_handle_irq_v4 */
+		rdmsrl(MSR_CORE_PERF_GLOBAL_STATUS, global);
+		pr_warn("PMI end - GLOBAL: %llx\n", global);
+		
+		goto again;
+	}
+
+	local_irq_restore(flags);
+
+end:
+	atomic_dec(&active_pmis);
+
+        return handled;
+}// handle_ibs_event
---
+++ driver/recode_logger.c
@@ -0,0 +1,56 @@
+#include "recode.h"
+
+struct pmc_logger *init_logger(unsigned cpu)
+{
+        struct pmc_logger *logger;
+        logger = vmalloc(sizeof(struct pmc_logger) + (BUFF_LENGTH * sizeof(struct pmcs_snapshot)));
+	if (!logger) 
+		return NULL;
+	reset_logger(logger);
+	logger->cpu = cpu;
+
+        return logger;
+}
+
+void reset_logger(struct pmc_logger *logger)
+{
+        if (!logger)
+                return;
+
+        logger->idx = 0;
+	logger->length = BUFF_LENGTH;
+}
+
+void fini_logger(struct pmc_logger *logger)
+{
+        vfree(logger);
+}
+
+int add_log(struct pmc_logger *logger, struct pmcs_snapshot *sample)
+{
+        if (!logger) {
+                pr_info("Internal error, logger is NULL\n");
+                return -1;
+        }
+        
+        if(logger->idx >= logger->length) {
+                pr_info("Cannot log on cpu %u, logger is FULL\n", logger->cpu);
+                return -1;
+        }
+
+        if (logger->idx % 128 == 0) {
+                pr_info("Registered %lu samples on core %u\n",
+                        logger->idx, logger->cpu);
+        }
+
+        memcpy(&logger->buff[logger->idx], sample, sizeof(typeof(*sample)));
+
+        logger->idx++;
+        return 0;
+}
+
+int flush_logs(struct pmc_logger *logger)
+{
+        /* TODO IMPLEMENT */
+        return 0;
+}
