198ea33d3e16a01e5a32a28b3614efba686d1035
Stefano Carnà <stefano.carna.dev@gmail.com>
2021-01-28 22:36:26 +0100

    Change decode from ascii to utf-8

--- tools/recode.py
+++ tools/recode.py
@@ -23,12 +23,12 @@ def cmd(cmd_seq, type=None, sh=False, timeOut=None):
         )
         p.wait(timeOut)
         if type == Pipe.OUT or type == Pipe.ERR:
-            return p.communicate()[type].decode("ASCII")
+            return p.communicate()[type].decode("utf-8")
         elif type == Pipe.RET:
             return p.returncode
         else:
             out, err = p.communicate()
-            return out.decode("ASCII"), err.decode("ASCII"), p.returncode
+            return out.decode("utf-8"), err.decode("utf-8"), p.returncode
     except subprocess.TimeoutExpired:
         print("Process automatically teminated after " + str(timeOut) + " sec")
         p.terminate()
